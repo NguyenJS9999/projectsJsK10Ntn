7933785b312030867ac23e00f6bd1105a80211cc
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-11-03 16:02:50 +0700

    copy balse logic category, detail

--- src/util/getParams.js
+++
@@ -1,7 +0,0 @@
-export function getParams(key) {
-	console.log('window.location.search ', window.location.search);
-	const params = new URLSearchParams(window.location.search);
-	console.log('getParams params: ', params);
-
-	return params.get(key);
-}
