775d0acbb7b4743dcc562d17a74579621cf486ae
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-11-04 20:16:09 +0700

    setup something libraly, make type lisst filter index

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,33 @@ import { getAll, getById, removeById, updateById } from "./services.js";
 import { getParams, render } from "./utils.js";
 
 const { products } = await getAll("products");
-console.log(products);
+products.sort((a, b) => a.rating - b.rating)
+const top5Products = products.slice(0, 5);
+// console.log('hotSaleSection: products', products);
+const hotSaleSection = document.getElementById("hot-sale-inner"); // rating
+render(hotSaleSection, top5Products);
 
-const hotSaleSection = document.getElementById("hot-sale");
 
-render(hotSaleSection, products);
+products.sort((a, b) => a.discountPercentage - b.discountPercentage)
+const topfiveProdiscount = products.slice(0, 5);
+console.log('hotSaleSection: topfiveProdiscount', topfiveProdiscount);
+const discountPercentageSection = document.getElementById("discount-percentage-inner"); // discountPercentage
+render(discountPercentageSection, topfiveProdiscount);
+
+// const Section = document.getElementById("hot-sale-inner"); // meta
+
+
+
 // render(hotSaleSection, products);
 
 const productId = getParams("id");
-
 const product = await getById("products", productId);
+
 
 // Cách 2: Lấy toàn bộ sản phẩm về -> products. Khi cần sản phẩm chi tiết thì sử dụng products.find()
 console.log(product);
+
+
+function getDetail() {
+    window.location = "";
+}
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -5,17 +5,24 @@ export function getParams(key) {
 }
 
 export function render(target, datas) {
-	datas.forEach((item) => {
-		const productElement = document.createElement("div");
+	datas.forEach(item => {
+		const productElement = document.createElement('div');
 		productElement.innerHTML = /*html*/ `
-        <div class="product-card">
-          <a href='/product-detail.html?id=${item.id}'><img src="${item.thumbnail}" alt="${item.title}" /></a>
-          <div class="product-infor">
-            <h2>${item.title}</h2>
-            <div>Giá: ${item.price}</div>
-            <p>Mô tả: ${item.description}</p>
-            <button class="btn btn-danger">Xem chi tiết</button>
-          </div>
+        <div class="product-item">
+            <a href='/product-detail.html?id=${item.id}'>
+                <img src="${item.thumbnail}" alt="${item.title}" />
+            </a>
+            <div class="product-infor">
+                <div class="prd-name shorten-text-two-line ">${item.title}</div>
+                <div class="prd-price shorten-text-two-line ">
+                    <span>Giá: </span>
+                    <span>${item.price}$</span>
+                </div>
+                <p class="prd-description shorten-text-two-line ">Mô tả: ${item.description}</p>
+
+                <button id="get-detail" class="btn btn-danger" onclick="getDetail()" >Xem chi tiết</button>
+
+            </div>
         </div>
     `;
 		target && target.appendChild(productElement);
